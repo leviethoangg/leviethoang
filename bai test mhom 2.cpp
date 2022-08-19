46738c28fa3209418aa99bc0043ffa69db8514c5
leviethoangg <111514749+leviethoangg@users.noreply.github.com>
2022-08-19 13:49:58 +0700

    https://fai.edunext.vn/vi/session/activity?sessionid=2464&activityId=1851

---
+++ bai test mhom 2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    int c = 10;
+    printf("12/6 * %d/2 = %d",c,12/6*c/2);
+    return 0;
+}
+
+
+
---
+++ main_l2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int num1;
+	int num2;
+	int s;
+	printf("dien gia tri tung bien: \n" );
+	scanf("%d",&num1);
+	scanf("%d",&num2);
+	s= num1 + num2;
+	printf("gia tri la: %d + %d = %d ",num1,num2,s);
+
+	return 0;
+}
